e90028f8d64efc40899086ebda89267fa8998f37
KevinMons1 <kevin2001@hotmail.be>
2022-08-02 16:26:54 +0200

    🔨 init scss

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-
-  </div>
+  
 </template>
 
 <script>
@@ -14,6 +12,6 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+  @import "./styles/_main";
 </style>
